050a16860033afb415a38c32330120f59a9ebaa5
yty <sdjn-yty@163.com>
2018-11-29 18:56:39 +0800

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DplayxDll;
+using System.Runtime.InteropServices;
+
+namespace Test
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var pdp= Marshal.AllocHGlobal(4);
+      var ret= Dplayx.DirectPlayCreate(IntPtr.Zero, pdp, IntPtr.Zero);
+      var dp = Marshal.ReadIntPtr(pdp);
+      var vtbl = Marshal.ReadIntPtr(dp);
+      Console.WriteLine($"{dp} {vtbl}");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ dplayx/Dplayx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllExporter;
+using System.Runtime.InteropServices;
+
+namespace DplayxDll
+{
+  public class Dplayx
+  {
+    [DllExport]
+    public static int DirectPlayCreate(IntPtr guid,IntPtr pDP,IntPtr unk)
+    {
+      var dp = Marshal.AllocHGlobal(IntPtr.Size*2);
+      Marshal.WriteIntPtr(pDP, dp);
+      var vtbl = dp + IntPtr.Size;
+      Marshal.WriteIntPtr(dp, vtbl);
+      var directPlay = new DirectPlay();
+      vtbl += IntPtr.Size;
+      var pF= Marshal.GetFunctionPointerForDelegate(new DirectPlay.QueryInterface(directPlay.QueryInterfaceI));
+      Marshal.WriteIntPtr(vtbl, pF);
+      return 0;
+    }
+
+    [DllExport]
+    public static void DirectPlayEnumerateA()
+    {
+
+    }
+
+    [DllExport]
+    public static void DirectPlayEnumerateW()
+    {
+
+    }
+
+    [DllExport]
+    public static void DirectPlayLobbyCreateA()
+    {
+
+    }
+  }
+
+  public class DirectPlay
+  {
+    public delegate int QueryInterface(IntPtr me, IntPtr ppvObj);
+
+    public delegate int AddRef(IntPtr me);
+
+    public delegate int Release(IntPtr me);
+
+    public int QueryInterfaceI(IntPtr me, IntPtr ppvObj)
+    {
+      return 0;
+    }
+  }
+}
